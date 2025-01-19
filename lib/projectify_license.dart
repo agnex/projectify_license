cf8c4230f8a74832816f214106c4ce0992c7ce81
Arun Gopinath <agnath18K@gmail.com>
2025-01-19 14:34:31 +0530

    path change

--- lib/projectify_license.dart
+++ lib/projectify_license.dart
@@ -57,7 +57,7 @@ class ProjectifyAnimation {
     // }
 
     return Lottie.asset(
-      'assets/animations/$animationName.json',
+      'packages/projectify_animations/assets/animations/$animationName.json',
       fit: fit,
       repeat: repeat,
       reverse: reverse,
